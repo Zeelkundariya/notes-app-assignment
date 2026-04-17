fb9e9db5be48c8c96cf5cd562921b2e90b59235b
Zeelkundariya <zeel.kundariya.m.cg@gmail.com>
2026-04-18 01:11:24 +0530

    feat: add create note endpoint (POST /api/notes)

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,4 +1,15 @@
 const Note = require('../models/note.model');
 
+//Q-1
+const createNote = async (req, res) => {
+    try {
+        const { title, content, category, isPinned } = req.body;
+        const note = new Note({ title, content, category, isPinned });
+        await note.save();
+        res.status(201).json({success: true, message: 'Note created successfully', data: note });
+    } catch (err) {
+        res.status(500).json({success: false, message: 'Error creating note', data: err });
+    }
+};
 
-module.exports = { };
+module.exports = { createNote };
--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {} = require('../controllers/note.controller');
+const {
+     createNote
+} = require('../controllers/note.controller');
 
 
+// CRUD routes
+router.post('/', createNote);
 
 module.exports = router;
